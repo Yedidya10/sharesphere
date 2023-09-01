c868043f8c8b136e1f83fc97145315153581fd4b
Yedidya Averjel <yedidya.abergel@gmail.com>
2023-09-01 16:44:37 +0300

    feat: update code

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,10 +1,17 @@
 import { MongoClient } from 'mongodb'
 
-if (!process.env.MONGODB_URI) {
+if (!process.env.MONGODB_URI && process.env.NODE_ENV === 'production') {
   throw new Error('Invalid/Missing environment variable: "MONGODB_URI"')
 }
 
-const uri = process.env.NODE_ENV === 'production' ? process.env.MONGODB_URI : process.env.MONGODB_URI_LOCAL || ''
+if (!process.env.MONGODB_URI_LOCAL && process.env.NODE_ENV === 'development') {
+  throw new Error('Invalid/Missing environment variable: "MONGODB_URI_LOCAL"')
+}
+
+const uri =
+  process.env.NODE_ENV === 'production'
+    ? process.env.MONGODB_URI
+    : process.env.MONGODB_URI_LOCAL || ''
 const options = {}
 
 let client
@@ -18,13 +25,13 @@ if (process.env.NODE_ENV === 'development') {
   }
 
   if (!globalWithMongo._mongoClientPromise) {
-    client = new MongoClient(uri, options)
+    client = new MongoClient(uri as string, options)
     globalWithMongo._mongoClientPromise = client.connect()
   }
   clientPromise = globalWithMongo._mongoClientPromise
 } else {
   // In production mode, it's best to not use a global variable.
-  client = new MongoClient(uri, options)
+  client = new MongoClient(uri as string, options)
   clientPromise = client.connect()
 }
 
--- src/models/dbModels.ts
+++ src/models/dbModels.ts
@@ -5,10 +5,13 @@ import { User } from './UserModel'
 mongoose.Promise = global.Promise
 
 // Get the MongoDB URI from the environment variables and connect to the database
-const mongoUri = process.env.MONGODB_URI!
+const mongoUri =
+  process.env.NODE_ENV === 'production'
+    ? process.env.MONGODB_URI
+    : process.env.MONGODB_URI_LOCAL || ''
 const connectToDatabase = async () => {
   try {
-    await mongoose.connect(mongoUri)
+    await mongoose.connect(mongoUri as string)
     console.log('Database connected')
   } catch (err: any) {
     console.error(`MongoDB connection error: ${err.message}`)
