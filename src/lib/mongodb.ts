4219ef1eede0cf05046685e2722cc0b7d3cec204
Yedidya Averjel <yedidya.abergel@gmail.com>
2023-09-01 16:43:46 +0300

    feat: update code

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -4,7 +4,7 @@ if (!process.env.MONGODB_URI) {
   throw new Error('Invalid/Missing environment variable: "MONGODB_URI"')
 }
 
-const uri = process.env.MONGODB_URI
+const uri = process.env.NODE_ENV === 'production' ? process.env.MONGODB_URI : process.env.MONGODB_URI_LOCAL || ''
 const options = {}
 
 let client
