3a0a65f0bc717eb843b1d9b478bd00a3d8dcfa41
Yedidya Averjel <yedidya.abergel@gmail.com>
2023-12-26 20:37:51 +0200

    fix: deleting irrelevant fields

--- src/components/cards/userOwnedCardInfo/UserOwnedCardInfo.mocks.ts
+++ src/components/cards/userOwnedCardInfo/UserOwnedCardInfo.mocks.ts
@@ -19,7 +19,6 @@ const base: IUserOwnedCardInfo = {
     details: {
       mainCategory: '',
       secondaryCategory: '',
-      tertiaryCategory: '',
       name: '',
       author: '',
       brand: '',
