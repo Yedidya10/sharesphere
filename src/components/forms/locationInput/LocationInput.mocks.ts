753bc9a12cb53887cbd9ade3b5f572823bc34180
Yedidya Averjel <yedidya.abergel@gmail.com>
2024-01-30 22:35:54 +0200

    feat: add functionality for using user's current location and main address

--- src/components/forms/locationInput/LocationInput.mocks.ts
+++ src/components/forms/locationInput/LocationInput.mocks.ts
@@ -1,9 +1,19 @@
+import { AddItemFormValues } from '@/utils/types/FormValues'
 import { ILocationInput } from './LocationInput'
 
 const base: ILocationInput = {
   label: '',
   control: {} as any,
   watch: {} as any,
+  setValue: function (
+    name: keyof AddItemFormValues,
+    value: any,
+    options?:
+      | Partial<{ shouldValidate: boolean; shouldDirty: boolean }>
+      | undefined
+  ): void {
+    throw new Error('Function not implemented.')
+  },
 }
 
 export const mockLocationInputProps = {
