ab3c9d29506c1f945130b6ac873aeef124d87bd7
Yedidya Averjel <yedidya.abergel@gmail.com>
2024-01-30 22:41:13 +0200

    feat: refactor user update and add user retrieval API endpoints

--- src/app/api/users/[id]/route.ts
+++ src/app/api/users/[id]/route.ts
@@ -3,16 +3,59 @@ import { User } from '@/models/dbModels'
 import { NextRequest, NextResponse } from 'next/server'
 
 // This is the API route that will be called when the form is submitted
-export async function PATCH(req: NextRequest) {
+export async function PUT(req: NextRequest) {
   try {
     // Get the data from the request body
-    const data = await req.json()
+    const updatedUser = await req.json()
+    const userId = req.nextUrl.href.split('/')[5]
 
     // Update the user with the given id
-    const user = await User.updateOne({ _id: data.id }, { $set: data })
+    const user = await User.findOneAndUpdate(
+      { _id: userId },
+      {
+        $set: {
+          // email: updatedUser.email,
+          firstName: updatedUser.firstName,
+          lastName: updatedUser.lastName,
+          phone: updatedUser.phone,
+          image: updatedUser.imageUrl,
+          'address.streetName': updatedUser.streetName,
+          'address.streetNumber': updatedUser.streetNumber,
+          'address.city': updatedUser.city,
+          'address.zipCode': updatedUser.zipCode,
+          'address.country': updatedUser.country,
+        },
+      },
+      { new: true }
+    )
+
+    if (!user) {
+      // If no user was found, return a 404 error
+      return NextResponse.json({ error: 'User not found' }, { status: 404 })
+    }
 
     // Return a 200 response
     return NextResponse.json({ message: 'user updated', user })
+  } catch (err) {
+    // If there is an error, return a 500 response with the error message
+    return NextResponse.json(
+      { error: 'Internal server error' },
+      { status: 500 }
+    )
+  }
+}
+
+export async function GET(req: NextRequest) {
+  try {
+    const userId = req.nextUrl.href.split('/')[5]
+    const user = await User.findOne({ _id: userId })
+    console.log(user)
+    if (!user) {
+      // If no user was found, return a 404 error
+      return NextResponse.json({ error: 'User not found' }, { status: 404 })
+    }
+    // Return a 200 response
+    return NextResponse.json({ message: 'user found', user })
   } catch (err) {
     // If there is an error, return a 500 response with the error message
     return NextResponse.json(
