cb06d19c7d38af421c039fce7f5a6265ecd8b16a
Yedidya Averjel <yedidya.abergel@gmail.com>
2024-08-03 23:21:06 +0300

    refactor: update NextAppDirEmotionCacheProvider to support RTL styling

--- src/components/ThemeRegistry/EmotionCache.tsx
+++ src/components/ThemeRegistry/EmotionCache.tsx
@@ -1,63 +1,72 @@
-'use client';
+'use client'
 
-import * as React from 'react';
-import createCache from '@emotion/cache';
-import { useServerInsertedHTML } from 'next/navigation';
-import { CacheProvider as DefaultCacheProvider } from '@emotion/react';
-import type { EmotionCache, Options as OptionsOfCreateCache } from '@emotion/cache';
-import rtlPlugin from 'stylis-plugin-rtl';
-import { CacheProvider } from '@emotion/react';
-import { prefixer } from 'stylis';
+import * as React from 'react'
+import createCache from '@emotion/cache'
+import { useServerInsertedHTML } from 'next/navigation'
+import { CacheProvider as DefaultCacheProvider } from '@emotion/react'
+import type {
+  EmotionCache,
+  Options as OptionsOfCreateCache,
+} from '@emotion/cache'
+import rtlPlugin from 'stylis-plugin-rtl'
+import { CacheProvider } from '@emotion/react'
+import { prefixer } from 'stylis'
 
 export type NextAppDirEmotionCacheProviderProps = {
   /** This is the options passed to createCache() from 'import createCache from "@emotion/cache"' */
-  options: Omit<OptionsOfCreateCache, 'insertionPoint'>;
+  options: Omit<OptionsOfCreateCache, 'insertionPoint'>
   /** By default <CacheProvider /> from 'import { CacheProvider } from "@emotion/react"' */
   CacheProvider?: (props: {
-    value: EmotionCache;
-    children: React.ReactNode;
-  }) => React.JSX.Element | null;
-  children: React.ReactNode;
-};
+    value: EmotionCache
+    children: React.ReactNode
+  }) => React.JSX.Element | null
+  children: React.ReactNode
+}
 
 // This implementation is taken from https://github.com/garronej/tss-react/blob/main/src/next/appDir.tsx
-export default function NextAppDirEmotionCacheProvider(props: NextAppDirEmotionCacheProviderProps) {
-  const { options, CacheProvider = DefaultCacheProvider, children } = props;
+export default function NextAppDirEmotionCacheProvider(
+  props: NextAppDirEmotionCacheProviderProps
+) {
+  const { options, CacheProvider = DefaultCacheProvider, children } = props
 
   const [{ cache, flush }] = React.useState(() => {
-    // eslint-disable-next-line 
-    const cache = createCache({
+    // eslint-disable-next-line
+    const rtlCache = createCache({
       key: 'muirtl',
       stylisPlugins: [prefixer, rtlPlugin],
-    });
-    cache.compat = true;
-    const prevInsert = cache.insert;
-    let inserted: string[] = [];
+    })
+    const ltrCache = createCache({
+      key: 'mui',
+    })
+    const cache = options.key === 'mui' ? ltrCache : rtlCache
+    cache.compat = true
+    const prevInsert = cache.insert
+    let inserted: string[] = []
     cache.insert = (...args) => {
-      const serialized = args[1];
+      const serialized = args[1]
       if (cache.inserted[serialized.name] === undefined) {
-        inserted.push(serialized.name);
+        inserted.push(serialized.name)
       }
-      return prevInsert(...args);
-    };
-    // eslint-disable-next-line 
+      return prevInsert(...args)
+    }
+    // eslint-disable-next-line
     const flush = () => {
-      const prevInserted = inserted;
-      inserted = [];
-      return prevInserted;
-    };
-    return { cache, flush };
-  });
+      const prevInserted = inserted
+      inserted = []
+      return prevInserted
+    }
+    return { cache, flush }
+  })
 
   useServerInsertedHTML(() => {
-    const names = flush();
+    const names = flush()
     if (names.length === 0) {
-      return null;
+      return null
     }
-    let styles = '';
+    let styles = ''
     // eslint-disable-next-line no-restricted-syntax
     for (const name of names) {
-      styles += cache.inserted[name];
+      styles += cache.inserted[name]
     }
     return (
       <style
@@ -68,8 +77,8 @@ export default function NextAppDirEmotionCacheProvider(props: NextAppDirEmotionC
           __html: styles,
         }}
       />
-    );
-  });
+    )
+  })
 
-  return <CacheProvider value={cache}>{children}</CacheProvider>;
+  return <CacheProvider value={cache}>{children}</CacheProvider>
 }
