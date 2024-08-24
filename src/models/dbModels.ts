9fca66a87e139beaff2459384b82f52d54c7bdcd
Yedidya Averjel <yedidya.abergel@gmail.com>
2024-08-25 01:29:57 +0300

    feat: update dbModels exports to include Notification and improve connectToDatabase func

--- src/models/dbModels.ts
+++ src/models/dbModels.ts
@@ -1,26 +1,39 @@
 import mongoose from 'mongoose'
 import { Item } from './ItemModel'
-import { User } from './UserModel'
+import { Notification } from './NotificationModel'
 import { Request } from './RequestModel'
+import { User } from './UserModel'
 
 mongoose.Promise = global.Promise
 
-// Get the MongoDB URI from the environment variables and connect to the database
 const mongoUri =
   process.env.NODE_ENV === 'production'
     ? process.env.MONGODB_URI
     : process.env.MONGODB_URI_LOCAL || ''
+
 const connectToDatabase = async () => {
+  if (mongoose.connection.readyState === 1) {
+    return
+  }
+
   try {
     await mongoose.connect(mongoUri as string)
-    console.log('Database connected')
   } catch (err: any) {
     console.error(`MongoDB connection error: ${err.message}`)
     throw new Error(err)
   }
 }
 
-// Call the connectToDatabase function to establish the connection
+// const closeDatabaseConnection = async () => {
+//   try {
+//     await mongoose.connection.close()
+//     console.log('Database connection closed')
+//   } catch (err: any) {
+//     console.error(`MongoDB connection error: ${err.message}`)
+//     throw new Error(err)
+//   }
+// }
+
 connectToDatabase()
 
-export { Item, User, Request }
+export { Item, Notification, Request, User }
