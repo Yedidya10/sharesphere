405c19c2f6d2e7cbd49a8c26f3ca89e72798002f
Yedidya Averjel <yedidya.abergel@gmail.com>
2024-08-28 01:42:05 +0300

    feat: add UserNotification component and related files

--- src/components/buttons/profileNotificationsButton/ProfileNotificationsButton.tsx
+++ src/components/buttons/profileNotificationsButton/ProfileNotificationsButton.tsx
@@ -1,6 +1,10 @@
+'use client'
+
+import UserNotification from '@/components/userNotification/UserNotification'
+import { Notification } from '@/utils/types/notification'
 import NotificationsIcon from '@mui/icons-material/Notifications'
+import Badge from '@mui/material/Badge'
 import Box from '@mui/material/Box'
-import Divider from '@mui/material/Divider'
 import IconButton from '@mui/material/IconButton'
 import Menu from '@mui/material/Menu'
 import MenuItem from '@mui/material/MenuItem'
@@ -8,7 +12,7 @@ import MenuList from '@mui/material/MenuList'
 import Tooltip from '@mui/material/Tooltip'
 import Typography from '@mui/material/Typography'
 import { useSession } from 'next-auth/react'
-import * as React from 'react'
+import { useEffect, useState } from 'react'
 
 export interface IProfileNotificationsButton {
   /**
@@ -38,116 +42,185 @@ const ProfileNotificationsButton: React.FC<IProfileNotificationsButton> = ({
   label,
 }) => {
   const { data: session, status } = useSession()
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null)
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
   const open = Boolean(anchorEl)
   const handleClick = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorEl(event.currentTarget)
+
+    if (unreadNotificationsNum > 0) {
+      const markAllAsRead = async () => {
+        for (const notification of notifications) {
+          if (notification.status === 'unread') {
+            await handleMarkAsRead(String(notification._id))
+          }
+        }
+      }
+      markAllAsRead()
+      setUnreadNotificationsNum(0)
+    }
   }
-  const handleClose = () => {
+  const [notifications, setNotifications] = useState<Notification[]>([])
+  const [unreadNotificationsNum, setUnreadNotificationsNum] = useState(0)
+  const handleCloseNotifications = () => {
     setAnchorEl(null)
+  }
+
+  useEffect(() => {
+    const fetchNotifications = async () => {
+      try {
+        const response = await fetch(
+          `/api/notifications/userId/${session?.user?.id}`,
+          {
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+          }
+        )
+
+        const data = await response.json()
+        const visibleNotifications = await data.filter(
+          (notification: Notification) => !notification.invisible
+        )
+
+        console.log('visibleNotifications:', visibleNotifications)
+        setNotifications(visibleNotifications)
+      } catch (error) {
+        console.error('Failed to fetch notifications')
+      }
+    }
+    if (session?.user?.id) {
+      fetchNotifications()
+    }
+  }, [session?.user?.id])
+
+  useEffect(() => {
+    if (status === 'authenticated' && notifications.length > 0) {
+      setUnreadNotificationsNum(
+        notifications.filter((notification) => notification.status === 'unread')
+          .length
+      )
+    }
+  }, [notifications, status])
+
+  const handleMarkAsRead = async (notificationId: string) => {
+    await fetch(`/api/notifications/${notificationId}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ status: 'read' }),
+    })
+  }
+
+  const handleHideNotification = async (notificationId: string) => {
+    await fetch(`/api/notifications/${notificationId}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ invisible: true }),
+    })
+
+    setNotifications((prev) =>
+      prev.filter(
+        (notification) => String(notification.user) !== notificationId
+      )
+    )
   }
 
   return (
     <>
       {status === 'authenticated' && (
-    
-      <Box sx={{ display: 'flex', alignItems: 'center', textAlign: 'center' }}>
-        <Tooltip title="Notifications">
-          <IconButton
-            onClick={handleClick}
-            size="small"
-            sx={{
-              padding: 1.5,
-            }}
-            aria-controls={open ? 'notifications-menu' : undefined}
-            aria-haspopup="true"
-            aria-expanded={open ? 'true' : undefined}
-          >
-            <NotificationsIcon />
-          </IconButton>
-        </Tooltip>
-      </Box>
-        
+        <Box
+          sx={{ display: 'flex', alignItems: 'center', textAlign: 'center' }}
+        >
+          <Tooltip title="Notifications">
+            <IconButton
+              onClick={handleClick}
+              size="small"
+              sx={{
+                padding: 1.5,
+              }}
+              aria-controls={open ? 'notifications-menu' : undefined}
+              aria-haspopup="true"
+              aria-expanded={open ? 'true' : undefined}
+            >
+              <Badge
+                badgeContent={unreadNotificationsNum}
+                color="error"
+                overlap="circular"
+              >
+                <NotificationsIcon color={primary ? 'primary' : 'inherit'} />
+              </Badge>
+            </IconButton>
+          </Tooltip>
+        </Box>
       )}
+
       <Menu
         anchorEl={anchorEl}
         id="notifications-menu"
         open={open}
-        onClose={handleClose}
-        onClick={handleClose}
+        onClose={handleCloseNotifications}
         transformOrigin={{ horizontal: 'right', vertical: 'top' }}
         anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
       >
         <MenuList sx={{ width: '100%', maxWidth: 460 }}>
-          <MenuItem
-            onClick={handleClose}
-            sx={{
-              display: 'flex',
-              alignItems: 'flex-start',
-              gap: 2,
-            }}
-          >
-            <NotificationsIcon sx={{ mt: 0.5 }} />
-            <Box
+          {notifications.length > 0 ? (
+            notifications.map((notification) => (
+              <>
+                <MenuItem
+                  // @ts-ignore
+                  key={notification.user}
+                  sx={{
+                    display: 'flex',
+                    alignItems: 'flex-start',
+                    gap: 2,
+                    paddingBlock: 1,
+                  }}
+                >
+                  <UserNotification
+                    title={notification.title}
+                    handleMarkAsRead={async () =>
+                      await handleMarkAsRead(String(notification._id))
+                    }
+                    handleCloseNotifications={handleCloseNotifications}
+                    handleHideNotification={async () =>
+                      await handleHideNotification(String(notification._id))
+                    }
+                    message={notification.message}
+                    image={notification.image}
+                    status={notification.status}
+                    label={''}
+                  />
+                </MenuItem>
+              </>
+            ))
+          ) : (
+            <MenuItem
               sx={{
-                maxWidth: 460,
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                gap: 2,
+                minWidth: 460,
+                minHeight: 200,
               }}
             >
               <Typography
-                sx={{
-                  fontWeight: '500',
-                }}
-              >
-                Notification 1
-              </Typography>
-              <Typography
                 variant="body2"
                 sx={{
+                  width: 'max-content',
                   textOverflow: 'ellipsis',
                   whiteSpace: 'normal',
+                  textAlign: 'center',
                 }}
               >
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-                ipsum purus, bibendum sit amet vulputate eget, porta semper
-                ligula.
+                No notifications
               </Typography>
-            </Box>
-          </MenuItem>
-          <Divider />
-          <MenuItem
-            onClick={handleClose}
-            sx={{
-              display: 'flex',
-              alignItems: 'flex-start',
-              gap: 2,
-            }}
-          >
-            <NotificationsIcon sx={{ mt: 0.5 }} />
-            <Box
-              sx={{
-                maxWidth: 460,
-              }}
-            >
-              <Typography
-                sx={{
-                  fontWeight: '500',
-                }}
-              >
-                Notification 2
-              </Typography>
-              <Typography
-                variant="body2"
-                sx={{
-                  textOverflow: 'ellipsis',
-                  whiteSpace: 'normal',
-                }}
-              >
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-                ipsum purus, bibendum sit amet vulputate eget, porta semper
-                ligula.
-              </Typography>
-            </Box>
-          </MenuItem>
+            </MenuItem>
+          )}
         </MenuList>
       </Menu>
     </>
